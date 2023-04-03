6ad11b773e78b4d400b55035163f4235d1285791
Saksham Gupta <sakshamgupta9656@gmail.com>
2023-04-03 21:55:27 +0530

    3-April-23

---
+++ Become 1 Star/Java/Number_Mirror.java
@@ -0,0 +1,17 @@
+// Problem Code: START01
+
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Number_Mirror
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        try (Scanner scn = new Scanner(System.in)) {
+            int num = scn.nextInt();
+            System.out.println(num);
+        }
+        
+	}
+}
